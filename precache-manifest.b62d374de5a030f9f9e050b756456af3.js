65dd3cb66ab9fd7abcd0eef8610f6eb988ce84ce
MaxVortman <maka.abort@gmail.com>
2018-11-14 13:09:52 +0300

    Updates

---
+++ precache-manifest.b62d374de5a030f9f9e050b756456af3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/Blitzkrieg/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "107899451aa7c50563fb",
+    "url": "/Blitzkrieg/static/js/runtime~main.10789945.js"
+  },
+  {
+    "revision": "fa54072db6a510c3cf28",
+    "url": "/Blitzkrieg/static/js/main.fa54072d.chunk.js"
+  },
+  {
+    "revision": "2dd891c45704ef41f3d5",
+    "url": "/Blitzkrieg/static/js/1.2dd891c4.chunk.js"
+  },
+  {
+    "revision": "fa54072db6a510c3cf28",
+    "url": "/Blitzkrieg/static/css/main.3946dfe0.chunk.css"
+  },
+  {
+    "revision": "4e066fa25eaff0625da7d4f7a44ac1e0",
+    "url": "/Blitzkrieg/index.html"
+  }
+];

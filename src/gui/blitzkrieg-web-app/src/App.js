62fa1e3b860762100bc5cf3006ec761c31c1b46e
MaxVortman <maka.abort@gmail.com>
2018-11-14 17:36:37 +0300

    simple interface implemented

--- src/gui/blitzkrieg-web-app/src/App.js
+++ src/gui/blitzkrieg-web-app/src/App.js
@@ -1,25 +1,21 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import logo from './img/blitzkrieg-text.png'
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+      <div className='app'>
+        <header className='app-header'>
+          <img className='header-logo' src={logo} alt='logo' />
         </header>
+        <div className='app-body'>
+          <textarea className='text-field' readOnly='true' />
+          <div className='app-submit-query'>
+            <input className="query-field" type='text' placeholder='Type a text...' />
+            <button className='submit-query-btn'>Submit</button>
+          </div>
+        </div>
       </div>
     );
   }
